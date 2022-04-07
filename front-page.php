5dfe82c1c4a66ab71389914c7d5b353c3b493364
widiyanata <widyanata48@gmail.com>
2022-04-07 23:45:39 +0700

    update front-page

--- front-page.php
+++ front-page.php
@@ -22,45 +22,49 @@ get_header();
             </div>
 
             <!-- Event -->
-            <div id="events">
+            <div id="events" class="">
                 <div class="title-event">
                     <h3>EVENTS</h3>
                 </div>
                 <div class="row">
                     <div class="col-md-4 d-flex align-items-center">
                         <div class="meta-tgl p-3 m-3 border">
-                            30 maret
+                            <img src="http://placehold.it/80" alt="">
                         </div>
                         <div class="event-text p-3">
                             <h3>Wisuda</h3>
                             <p>GSG Universitas Unila</p>
+                            <small>30 Maret</small>
                         </div>
                     </div>
                     <div class="col-md-4 d-flex align-items-center">
                         <div class="meta-tgl p-3 m-3 border">
-                            30 maret
+                            <img src="http://placehold.it/80" alt="">
                         </div>
                         <div class="event-text p-3">
                             <h3>Wisuda</h3>
                             <p>GSG Universitas Unila</p>
+                            <small>30 Maret</small>
                         </div>
                     </div>
                     <div class="col-md-4 d-flex align-items-center">
                         <div class="meta-tgl p-3 m-3 border">
-                            30 maret
+                            <img src="http://placehold.it/80" alt="">
                         </div>
                         <div class="event-text p-3">
                             <h3>Wisuda</h3>
                             <p>GSG Universitas Unila</p>
+                            <small>30 Maret</small>
                         </div>
                     </div>
                     <div class="col-md-4 d-flex align-items-center">
                         <div class="meta-tgl p-3 m-3 border">
-                            30 maret
+                            <img src="http://placehold.it/80" alt="">
                         </div>
                         <div class="event-text p-3">
                             <h3>Wisuda</h3>
                             <p>GSG Universitas Unila</p>
+                            <small>30 Maret</small>
                         </div>
                     </div>
                 </div>
